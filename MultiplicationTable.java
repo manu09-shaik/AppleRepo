1417baaa3ab1c0b530a8883496b9b44eb0d5de76
root <root@slave.com>
2017-07-10 12:37:25 -0700

    second commit

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+public class MultiplicationTable {
+
+	public void tableDisplay(){		
+		Scanner scanner = new Scanner(System.in);
+		int n =5;
+		for (int i = 1; i <= 10; i++) {
+			 System.out.println(n+" * "+i+" = "+n*i);
+		}
+		scanner.close();
+	}
+	public static void main(String[] args) {
+		System.out.println( " Inside main method of MultiplicationTable ");
+		MultiplicationTable table = new MultiplicationTable();
+		table.tableDisplay();
+	}
+}
